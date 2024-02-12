6e6077c0edfc2bd0208fba21955b94c51c30bb94
Thomas Morton <tommeltot@icloud.com>
2024-02-12 16:39:34 +0000

    Main popup responsible for all settings

--- extension/inject.js
+++ extension/inject.js
@@ -1,14 +1,28 @@
 var pieces = ["wp", "wb", "wn", "wr", "wq", "wk", "bp", "bb", "bn", "br", "bq", "bk"];
 
-window.onload = function() {
+function changePieces() {
     let root = document.documentElement;
 
     for (let i = 0; i < pieces.length; i++) {
+        let elements = document.getElementsByClassName(`piece ${pieces[i]}`);
+
         chrome.storage.local.get(pieces[i], function(result) {
             let url = result[pieces[i]];
             if (url) {
                 root.style.setProperty(`--theme-piece-set-${pieces[i]}`, `url(${url})`);
+
+                for (let j = 0; j < elements.length; j++) {
+                    elements[j].style.backgroundImage = `url(${url})`;
+                }
             }
         });
     }
+};
+
+window.onload = function() {
+    // Create an observer instance linked to the callback function
+    let observer = new MutationObserver(changePieces);
+
+    // Start observing the document body for configured mutations
+    observer.observe(document.body, { childList: true, subtree: true });
 };
--- popup/script.js
+++ popup/script.js
@@ -1,16 +1,18 @@
 var pieces = ["wp", "wb", "wn", "wr", "wq", "wk", "bp", "bb", "bn", "br", "bq", "bk"];
 var preview = document.getElementById("preview-pieces");
 
-for (let i = 0; i < pieces.length; i++) {
-    chrome.storage.local.get(pieces[i], function(result) {
-        var value = result[pieces[i]];
-        if (value) {
-            preview.innerHTML += `<div class=\"piece\"><img src=\"${value}\"></div>`;
-        }
-        else {
-            preview.innerHTML += `<div class=\"piece\"><img src="../icons/empty.png"></div>`;
-        }
-    });
+function showPieces() {
+    for (let i = 0; i < pieces.length; i++) {
+        chrome.storage.local.get(pieces[i], function(result) {
+            var value = result[pieces[i]];
+            if (value) {
+                preview.innerHTML += `<div class=\"piece\"><img src=\"${value}\"></div>`;
+            }
+            else {
+                preview.innerHTML += `<div class=\"piece\"><img src="../icons/empty.png"></div>`;
+            }
+        });
+    }
 }
 
 document.getElementById("reset").addEventListener(
@@ -24,3 +26,52 @@ document.getElementById("reset").addEventListener(
         }
     }
 );
+
+document.getElementById("upload-pieces").addEventListener(
+    "change",
+    (event) => {
+        // let log = document.getElementById("result");
+
+        for (const file of event.target.files) {
+            let id = file.name.slice(0, 2).toLowerCase();
+
+            if (!pieces.includes(id)) {
+                // log.innerHTML += "<li class=\"skip\">Skipped '" + file.name + "' (bad name)</li>";
+            }
+            else if (!file.name.includes(".png")  &&
+                     !file.name.includes(".jpg")  &&
+                     !file.name.includes(".jpeg") &&
+                     !file.name.includes(".svg"))
+            {
+                // log.innerHTML += "<li class=\"skip\">Skipped '" + file.name + "' (bad type)</li>";
+            }
+            else {
+                // Create a new FileReader for each file.
+                /*
+                 * The FileReader API is asynchronous, so when you’re trying to read multiple files
+                 * in a loop, the load event might be triggered for a different file than you expect.
+                 */
+                const reader = new FileReader();
+
+                // Convert image to data URL.
+                reader.readAsDataURL(file);
+                reader.addEventListener("load", () => {
+                    // Save data URL to chrome.storage.
+                    let obj = {};
+                    obj[id] = reader.result;
+                    chrome.storage.local.set(obj, function() {
+                        // console.log("Value is set to " + reader.result);
+                    });
+                    // log.innerHTML += "<li class=\"accept\">Accepted '" + file.name + "'</li>";
+                });
+            }
+        }
+        preview.innerHTML = "";
+        showPieces();
+    },
+    false
+);
+
+window.onload = function() {
+    showPieces();
+};
