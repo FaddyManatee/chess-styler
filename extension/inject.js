37ac1b08eae4bb7fa6d6779badf0ac15f7f54c3e
Thomas Morton <tommeltot@icloud.com>
2024-02-12 13:34:55 +0000

    Pieces are now successfully injected, but introduces unwanted board changes

--- extension/inject.js
+++ extension/inject.js
@@ -1,16 +1,14 @@
 var pieces = ["wp", "wb", "wn", "wr", "wq", "wk", "bp", "bb", "bn", "br", "bq", "bk"];
 
 window.onload = function() {
-  for (let i = 0; i < pieces.length; i++) {
-    let elements = document.getElementsByClassName(`piece ${pieces[i]}`);
+    let root = document.documentElement;
 
-    chrome.storage.local.get(pieces[i], function(result) {
-      let url = result[pieces[i]];
-      if (url) {
-        for (let j = 0; j < elements.length; j++) {
-          elements[j].style.backgroundImage = `url(${url})`;
-        }
-      }
-    });
-  }
+    for (let i = 0; i < pieces.length; i++) {
+        chrome.storage.local.get(pieces[i], function(result) {
+            let url = result[pieces[i]];
+            if (url) {
+                root.style.setProperty(`--theme-piece-set-${pieces[i]}`, `url(${url})`);
+            }
+        });
+    }
 };
